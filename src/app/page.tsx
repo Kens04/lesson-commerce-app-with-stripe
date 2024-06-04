e2d595cd20aa0d3b9a452c74b2858112256329eb
Kens04 <kento090403@icloud.com>
2024-06-04 22:14:50 +0900

    関数宣言の追加とSSGとSSRについて

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,14 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Link from "next/link";
 
-export default async function Home() {
-  const supabase = createServerComponentClient({ cookies });
+const supabase = createServerComponentClient({ cookies });
+const getAllLessons = async () => {
   const { data: lessons } = await supabase.from("lesson").select("*");
+  return lessons;
+};
+
+export default async function Home() {
+  const lessons = await getAllLessons();
 
   return (
     <main className="w-full max-w-3xl mx-auto my-16 px-2">
