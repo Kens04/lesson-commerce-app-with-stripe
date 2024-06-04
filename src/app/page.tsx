23907f9fc7995a682c64b841300e0a29dae41175
Kens04 <kento090403@icloud.com>
2024-06-04 22:11:33 +0900

    shadcn/uiを使ってカードを作る

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Link from "next/link";
@@ -7,12 +8,21 @@ export default async function Home() {
   const { data: lessons } = await supabase.from("lesson").select("*");
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {lessons?.map((lesson) => (
-        <Link href={`/${lesson.id}`} key={lesson.id}>
-          {lesson.title}
-        </Link>
-      ))}
+    <main className="w-full max-w-3xl mx-auto my-16 px-2">
+      <div className="flex flex-col gap-3">
+        {lessons?.map((lesson) => (
+          <Link href={`/${lesson.id}`} key={lesson.id}>
+            <Card>
+              <CardHeader>
+                <CardTitle>{lesson.title}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>{lesson.description}</p>
+              </CardContent>
+            </Card>
+          </Link>
+        ))}
+      </div>
     </main>
   );
 }
