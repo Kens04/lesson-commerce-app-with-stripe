c12988cc00e6ccf61f832daf99add098dd93e506
Kens04 <kento090403@icloud.com>
2024-06-09 08:40:52 +0900

    Stripe WebHooksを作成

---
+++ src/app/api/stripe-hooks/route.ts
@@ -0,0 +1,59 @@
+// // server.js
+// //
+// // Use this sample code to handle webhook events in your integration.
+// //
+// // 1) Paste this code into a new file (server.js)
+// //
+// // 2) Install dependencies
+// //   npm install stripe
+// //   npm install express
+// //
+// // 3) Run the server on http://localhost:4242
+// //   node server.js
+
+// // The library needs to be configured with your account's secret key.
+// // Ensure the key is kept out of any version control system you might be using.
+// const stripe = require('stripe')('sk_test_...');
+// const express = require('express');
+// const app = express();
+
+
+// // This is your Stripe CLI webhook secret for testing your endpoint locally.
+// const endpointSecret = "whsec_829513b2cb58557327860ab5ba218063bb20112796b26e6a6646438a0986eacb";
+
+// app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+//   const sig = request.headers['stripe-signature'];
+
+//   let event;
+
+//   try {
+//     event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+//   } catch (err) {
+//     response.status(400).send(`Webhook Error: ${err.message}`);
+//     return;
+//   }
+
+//   // Handle the event
+//   switch (event.type) {
+//     case 'customer.subscription.created':
+//       const customerSubscriptionCreated = event.data.object;
+//       // Then define and call a function to handle the event customer.subscription.created
+//       break;
+//     case 'customer.subscription.deleted':
+//       const customerSubscriptionDeleted = event.data.object;
+//       // Then define and call a function to handle the event customer.subscription.deleted
+//       break;
+//     case 'customer.subscription.updated':
+//       const customerSubscriptionUpdated = event.data.object;
+//       // Then define and call a function to handle the event customer.subscription.updated
+//       break;
+//     // ... handle other event types
+//     default:
+//       console.log(`Unhandled event type ${event.type}`);
+//   }
+
+//   // Return a 200 response to acknowledge receipt of the event
+//   response.send();
+// });
+
+// app.listen(4242, () => console.log('Running on port 4242'));
