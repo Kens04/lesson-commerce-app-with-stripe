fcaaac3fe90b4007e5870856fdeaf1536a8c279b
Kens04 <kento090403@icloud.com>
2024-06-08 23:17:50 +0900

    決済成功ページとキャンセルページを作成

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -16,6 +16,7 @@ import { cookies } from "next/headers";
 import { profile } from "console";
 import { Database } from "@/lib/database.types";
 import SubscriptionButton from "@/components/checkout/SubscriptionButton";
+import AuthServerButton from "@/components/auth/AuthServerButton";
 
 interface Plan {
   id: string;
@@ -82,7 +83,7 @@ const PricingPage = async () => {
           </CardContent>
           <CardFooter>
             {showSubscribeButton && <SubscriptionButton planId={plan.id} />}
-            {showCreateAccountButton && <Button>ログインする</Button>}
+            {showCreateAccountButton && <AuthServerButton />}
             {showManageSubscriptionButton && (
               <Button>サブスクリプション管理する</Button>
             )}
