f31bacc8f618be76ec7f351b6aeae6ce55cd4775
Kens04 <kento090403@icloud.com>
2024-06-09 09:03:47 +0900

    WebHooksの通知を受け取るAPIを実装

--- src/app/api/stripe-hooks/route.ts
+++ src/app/api/stripe-hooks/route.ts
@@ -1,44 +1,42 @@
 import { NextRequest, NextResponse } from "next/server";
+import initStripe from "stripe";
 
 export async function POST(req: NextRequest) {
-  console.log("webhook dispatch");
+  const stripe = new initStripe(process.env.Stripe_SECRET_KEY!);
+  const signature = req.headers.get("stripe-signature");
+  const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
 
-  return NextResponse.json({ received: true });
-}
-
-const stripe = require('stripe')('sk_test_...');
-
-
-// This is your Stripe CLI webhook secret for testing your endpoint locally.
-const endpointSecret = "whsec_829513b2cb58557327860ab5ba218063bb20112796b26e6a6646438a0986eacb";
-
-
-  const sig = request.headers['stripe-signature'];
+  const reqBuffer = Buffer.from(await req.arrayBuffer());
 
   let event;
 
   try {
-    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-  } catch (err) {
-    response.status(400).send(`Webhook Error: ${err.message}`);
-    return;
-  }
+    event = stripe.webhooks.constructEvent(
+      reqBuffer,
+      signature!,
+      endpointSecret!
+    );
+    // switch (event.type) {
+    //   case "customer.subscription.created":
+    //     const customerSubscriptionCreated = event.data.object;
+    //     // Then define and call a function to handle the event customer.subscription.created
+    //     break;
+    //   case "customer.subscription.deleted":
+    //     const customerSubscriptionDeleted = event.data.object;
+    //     // Then define and call a function to handle the event customer.subscription.deleted
+    //     break;
+    //   case "customer.subscription.updated":
+    //     const customerSubscriptionUpdated = event.data.object;
+    //     // Then define and call a function to handle the event customer.subscription.updated
+    //     break;
+    //   // ... handle other event types
+    //   default:
+    //     console.log(`Unhandled event type ${event.type}`);
+    // }
 
-  // Handle the event
-  switch (event.type) {
-    case 'customer.subscription.created':
-      const customerSubscriptionCreated = event.data.object;
-      // Then define and call a function to handle the event customer.subscription.created
-      break;
-    case 'customer.subscription.deleted':
-      const customerSubscriptionDeleted = event.data.object;
-      // Then define and call a function to handle the event customer.subscription.deleted
-      break;
-    case 'customer.subscription.updated':
-      const customerSubscriptionUpdated = event.data.object;
-      // Then define and call a function to handle the event customer.subscription.updated
-      break;
-    // ... handle other event types
-    default:
-      console.log(`Unhandled event type ${event.type}`);
+    console.log(event);
+    return NextResponse.json({ received: true });
+  } catch (err: any) {
+    return NextResponse.json(`{Webhook Error: ${err.message}`, { status: 401 });
   }
+}
